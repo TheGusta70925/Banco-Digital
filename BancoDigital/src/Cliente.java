dd6102fafe7854f615403807508656a1627938cc
TheGusta <lg70925@gmail.com>
2024-09-04 18:30:08 -0300

    testando

--- BancoDigital/src/Cliente.java
+++ BancoDigital/src/Cliente.java
@@ -1,13 +1,29 @@
 public class Cliente {
 
 	private String nome;
+    private String data;
+    private String sexo;
 
-	public String getNome() {
+    
+	public Cliente(String nome, String data, String sexo) {
+        this.nome = nome;
+        this.data = data;
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Informação do cliente [nome=" + nome + ", data=" + data + ", sexo=" + sexo + "]";
+    }
+
+    public String getNome() {
 		return nome;
 	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getData() {
+        return data;
+    }
+    public String getSexo() {
+        return sexo;
+    }
 
 }
--- BancoDigital/src/Main.java
+++ BancoDigital/src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente venilton = new Cliente("Venilton", "21/02/1997", "Masculino");
 		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
+
+        System.out.println(venilton.toString());
+        System.out.println("");
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
+        
 	}
 
 }
